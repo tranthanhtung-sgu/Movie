a8139bfea343392f226852424e2a741c7ef40b50
Trần Thanh Tùng <50178152+tranthanhtung-sgu@users.noreply.github.com>
2021-06-19 19:47:19 +0700

    make it better final 9

--- Application/Movies/MovieParams.cs
+++ Application/Movies/MovieParams.cs
@@ -3,7 +3,7 @@ using Application.Core;
 namespace Application.Movies
 {
     public class MovieParams : PagingParams
-    {
+    { 
         public string Category { get; set; }
         public string Title { get; set; }
     }
